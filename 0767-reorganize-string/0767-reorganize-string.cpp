a4066df8eea06035485e63e659dbc280fd9b22fd
Ankit Kumar <ankitkumar8317@gmail.com>
2024-03-08 07:14:13 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (44.75%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -3,19 +3,26 @@ public:
     string reorganizeString(string s) {
        vector<int> chrCounts(26);
 		for (auto& chr: s) { if(++chrCounts[chr-97] > (s.size()+1)/2) return "";}
-		priority_queue<pair<int, char>> max_heap;
-		for (int i = 0; i < 26; i++) {
-			if (chrCounts[i]) {max_heap.push({chrCounts[i], 97+i});}
-		}
-		int i = 0;
-		pair<int, char> prev = max_heap.top(); max_heap.pop();
-		s[i++] = prev.second;
-		while (not max_heap.empty()) {
-			pair<int, char> curr = max_heap.top(); max_heap.pop();
-			s[i++] = curr.second;
-			if (--prev.first) max_heap.push(prev);
-			prev = curr;
-		}
-		return s;
+        
+        priority_queue<pair<int,char>> pq;
+        
+        for(int i=0;i<26;i++){
+            if(chrCounts[i]){
+                pq.push({chrCounts[i],i+97});
+            }
+        }
+        int i=0;
+        pair<int,char> prev = pq.top();
+        pq.pop();
+        s[i++]=prev.second;
+        
+        while(not pq.empty()){
+            pair<int,char> curr = pq.top();
+            pq.pop();
+            s[i++]=curr.second;
+            if(--prev.first)pq.push(prev);
+            prev=curr;
+        }
+        return s;
     }
 };
