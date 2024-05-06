cf9464979b9a11e6b8d374441bc0b35488e1dc37
Ankit Kumar <ankitkumar8317@gmail.com>
2024-05-06 12:59:41 +0530

    Time: 7 ms (90.05%), Space: 9.3 MB (6.54%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,6 +1,11 @@
+unordered_set<int> s;
+bool f = []{
+    for (uint i = 0, powOf3 = 1; i <= 19; i++, powOf3 *= 3) s.insert(powOf3);    
+    return true;
+}(); 
 class Solution {
-public:
+public:    
     bool isPowerOfThree(int n) {
-        return n > 0 && 1162261467 % n == 0;
+        return s.find(n) != end(s);
     }
 };
