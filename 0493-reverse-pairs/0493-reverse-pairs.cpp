74ec91344f59e700823f0534a9b844b087bf6b13
Ankit Kumar <ankitkumar8317@gmail.com>
2024-05-07 06:14:15 +0530

    Time: 259 ms (72.69%), Space: 109.4 MB (79.97%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void merge(vector<int>&nums,int low,int mid,int high,int& count){
+        int j=mid+1;
+        for(int i=low;i<=mid;i++){
+            while(j<=high && nums[i]>2*(long long)nums[j]){
+                j++;
+            }
+            count+=j-(mid+1);
+        }
+        int size = high-low+1;
+        vector<int> temp(size,0);
+        int left=low,right=mid+1,k=0;
+        
+        while(left<=mid && right<=high){
+            if(nums[left]<nums[right]) {
+                temp[k++]=nums[left++];
+            }else {
+                temp[k++]=nums[right++];
+            }
+        }
+        while(left<=mid){
+            temp[k++]=nums[left++];
+        }
+        while(right<=high){
+            temp[k++]=nums[right++];
+        }
+        int m=0;
+        for(int i=low;i<=high;i++){
+            nums[i]=temp[m++];
+        }
+    }
+    void mergeSort(vector<int>&nums, int low,int high,int& count){
+        if(low>=high)return;
+        int mid = (low+high)>>1;
+        mergeSort(nums,low,mid,count);
+        mergeSort(nums,mid+1,high,count);
+        merge(nums,low,mid,high,count);
+    }
+    int reversePairs(vector<int>& nums) {
+        int count = 0;
+        mergeSort(nums,0,nums.size()-1,count);
+        return count;
+    }
+};
