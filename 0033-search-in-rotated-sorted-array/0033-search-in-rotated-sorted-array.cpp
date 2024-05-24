31c02cb31fac62dc7d85fbc56a4614c993391e9a
Ankit Kumar <ankitkumar8317@gmail.com>
2024-05-24 20:46:11 +0530

    Time: 0 ms (100.00%), Space: 13.4 MB (77.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int ans = -1;
-        int s = 0,e=nums.size()-1;
+        int n = nums.size();
+        
+        int s=0,e=n-1;
         
         while(s<=e){
             int mid = s+(e-s)/2;
+           // cout<<"mid"<<mid<<endl;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]>=nums[0]){
-                if(nums[s]<=target && nums[mid]>=target) {
-                    e=mid-1;
+            }
+            if(nums[0]<=nums[mid]){
+               // cout<<"0"<<nums[mid]<<endl;
+                if(target>=nums[0] && target<=nums[mid]) {
+                    //cout<<'s'<<endl;
+                    e = mid-1;
                 }else{
-                    s=mid+1;
+                    //cout<<"aa"<<endl;
+                    s = mid+1;
                 }
             }else{
-                if(nums[mid]<=target && nums[e]>=target) {
+                if(target>=nums[mid] && target<=nums[e]){
                     s=mid+1;
                 }else{
                     e=mid-1;
                 }
             }
         }
-        return ans;
+        return -1;
     }
 };
