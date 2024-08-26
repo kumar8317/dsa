a8dfc0f156670973f8b6315a9b3580f3e448a495
Ankit Kumar <ankitkumar8317@gmail.com>
2024-08-26 19:24:58 +0530

    Time: 24 ms (64.11%), Space: 15.9 MB (87.64%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& mat, int k) {
-        vector<pair<int,pair<int,int>>> temp;
-        int n = mat.size();
-    
-          for(int i=0;i<n;i++){
-              temp.push_back(make_pair(mat[i][0],make_pair(i,0)));
-          }
-          priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>               pq(temp.begin(),temp.end());
-    
-          int ans;
-          int i,j;
-          pair<int,pair<int,int>> element;
-          while(k--){
-              element = pq.top();
-              pq.pop();
-              ans = element.first;
-              i = element.second.first;
-              j = element.second.second;
-              if(j+1 < n){
-                  pq.push(make_pair(mat[i][j+1],make_pair(i,j+1)));
-              }
-          }
-          return ans;
+    int countElements(vector<vector<int>>& matrix,int ele){
+        int cols = matrix[0].size();
+        int count = 0;
+        for(int r = 0,c =cols-1;r<matrix.size();r++){
+            while(c>=0 && matrix[r][c]> ele)c--;
+            
+            if(c>=0){
+                count+=c+1;
+                
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        int n = matrix.size();
+        int start=matrix[0][0];
+        int end = matrix[n-1][n-1];
+        int ans;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            
+            int count = countElements(matrix,mid);
+            if(count>=k){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return ans;
     }
 };
