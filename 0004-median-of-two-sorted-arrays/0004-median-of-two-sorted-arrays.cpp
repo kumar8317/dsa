c4f985de8d95cd28bb609471e8fe8f255b126376
Ankit Kumar <ankitkumar8317@gmail.com>
2024-09-01 11:27:17 +0530

    Time: 27 ms (48.00%), Space: 94.9 MB (51.29%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size()){
-            return findMedianSortedArrays(nums2,nums1);
-        }
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        int total = n1+n2;
-        int half = (total+1)/2;
+        if(n1>n2)return findMedianSortedArrays(nums2,nums1);
         
+        int total = n1+n2;
+        
+        int half = (total+1)/2;
         int l=0,r=n1;
         
         while(l<=r){
-            int mid = l+(r-l)/2; //nums1
-            int j = half - mid; //nums2
+            int i = l+(r-l)/2; //nums1
+            int j = half-i; //nums2
             
-            int Aleft = INT_MIN;
-            int Aright = INT_MAX;
-            int Bleft = INT_MIN;
-            int Bright = INT_MAX;
+            int Aleft = i>0 ? nums1[i-1] : INT_MIN;
+            int Aright = i<n1? nums1[i] : INT_MAX;
             
-            if(mid>0)Aleft = nums1[mid-1];
-            if(mid < n1) Aright = nums1[mid];
+            int Bleft = j>0 ? nums2[j-1] : INT_MIN;
+            int Bright = j<n2 ? nums2[j]: INT_MAX;
             
-            if(j>0)Bleft = nums2[j-1];
-            if(j<n2)Bright = nums2[j];
-            
-            if((Aleft <= Bright) && (Bleft <= Aright)) {
+            if((Aleft <= Bright) && (Bleft <= Aright)){
                 if(total%2 != 0){
                     return max(Aleft,Bleft);
                 }else{
-                    return (max(Aleft,Bleft)+min(Aright,Bright)) / 2.0;
+                    return (max(Aleft,Bleft)+min(Aright,Bright))/2.0 ;
                 }
-            }else if( Aleft > Bright){
-                r = mid-1;
-            }else {
-                l = mid+1;
+            }else if(Aleft > Bright){
+                r = i-1;
+            }else{
+                l = i+1;
             }
         }
         
         return 0.0;
-        
     }
 };
