e0978ada2d9d632874aa19baa162fafbd2b91b7b
Ankit Kumar <ankitkumar8317@gmail.com>
2024-09-01 11:16:22 +0530

    Time: 39 ms (7.16%), Space: 94.5 MB (87.63%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n1 = nums1.size();
         int n2 = nums2.size();
         
-        if(n1>n2)return findMedianSortedArrays(nums2,nums1);
+        int total = n1+n2;
+        int half = (total+1)/2;
         
-        int n = n1+n2;
+        int l=0,r=n1;
         
-        int left = (n1+n2+1)/2;
-        
-        int low=0,high=n1;
-        
-        while(low<=high){
-            int mid1 = (low+high)/2;
-            int mid2 = left-mid1;
+        while(l<=r){
+            int mid = l+(r-l)/2; //nums1
+            int j = half - mid; //nums2
             
-            int l1=INT_MIN,l2=INT_MIN;
-            int r1 = INT_MAX, r2 = INT_MAX;
+            int Aleft = INT_MIN;
+            int Aright = INT_MAX;
+            int Bleft = INT_MIN;
+            int Bright = INT_MAX;
             
-            if(mid1<n1){
-                r1 = nums1[mid1];
-            }
-            if(mid2<n2){
-                r2 = nums2[mid2];
-            }
+            if(mid>0)Aleft = nums1[mid-1];
+            if(mid < n1) Aright = nums1[mid];
             
-            if(mid1-1>=0){
-                l1=nums1[mid1-1];
-            }
-            if(mid2-1>=0){
-                l2 = nums2[mid2-1];
-            }
+            if(j>0)Bleft = nums2[j-1];
+            if(j<n2)Bright = nums2[j];
             
-            if(l1<=r2 && l2<=r1){
-                if(n%2==1){
-                    return (double)max(l1,l2);
+            if((Aleft <= Bright) && (Bleft <= Aright)) {
+                if(total%2 != 0){
+                    return max(Aleft,Bleft);
                 }else{
-                    return ((double)(max(l1,l2)+min(r1,r2)))/2.0;
+                    return (max(Aleft,Bleft)+min(Aright,Bright)) / 2.0;
                 }
+            }else if( Aleft > Bright){
+                r = mid-1;
+            }else {
+                l = mid+1;
             }
-            else if(l1>r2)high = mid1-1;
-            else low = mid1+1;
         }
-        return 0;
+        
+        return 0.0;
+        
     }
 };
