490711ff14e2f52be9ce3620da9e3d1d7e025ced
Ankit Kumar <ankitkumar8317@gmail.com>
2024-09-20 20:36:54 +0530

    Time: 70 ms (98.15%), Space: 71.9 MB (93.10%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int uniqueTree = 0;
+        int left=0,right=0,ans=0;
+        int n = fruits.size();
+        vector<int> mp(n,0);
+        while(right<n){
+            mp[fruits[right]]++;
+            if(mp[fruits[right]]==1)uniqueTree++;
+            
+            while(uniqueTree>2){
+               if(mp[fruits[left]]==1)uniqueTree--;
+                mp[fruits[left]]--;
+                left++;
+            }
+            ans = max(ans,right-left+1);
+            right++;
+        }
+        return ans;
+    }
+};
