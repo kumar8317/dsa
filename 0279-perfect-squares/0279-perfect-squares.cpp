a8680ad79923da49012062d2401d832b7bf079ed
Ankit Kumar <ankitkumar8317@gmail.com>
2024-09-21 12:54:07 +0530

    Time: 48 ms (67.87%), Space: 53.1 MB (12.03%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -11,7 +11,7 @@ public:
             for(int i = 0; i < size; i++){
                 int node = q.front();
                 q.pop();
-                for(int j = 0; (j*j) <= node; j++){
+                for(int j = 1; (j*j) <= node; j++){
                     int QueueInsert =  node - j*j;
                     if(QueueInsert == 0){
                         return step;
