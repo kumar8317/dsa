1ff53c833ef3b2ef5593070c08faf4d5a865798f
Ankit Kumar <ankitkumar8317@gmail.com>
2024-09-21 12:49:30 +0530

    Time: 76 ms (52.77%), Space: 71.8 MB (11.32%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        queue<int> q;
+        int step = 1;
+        q.push(n);
+    
+        while(!q.empty()){
+            
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                int node = q.front();
+                q.pop();
+                for(int j = 0; (j*j) <= node; j++){
+                    int QueueInsert =  node - j*j;
+                    if(QueueInsert == 0){
+                        return step;
+                    }
+                    q.push(QueueInsert);
+                }
+            }
+            step++;
+        }
+
+        return step;
+
+
+        
+        
+
+        
+    }
+};
